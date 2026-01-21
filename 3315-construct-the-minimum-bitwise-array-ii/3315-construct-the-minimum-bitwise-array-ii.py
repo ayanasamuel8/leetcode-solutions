03fcdfc244d5402a0018ccdf399bc168ffeb1dd1
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2026-01-21 20:25:17 +0300

    Time: 0 ms (100%), Space: 19.4 MB (20.75%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.py
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.py
@@ -4,17 +4,15 @@ class Solution:
         n = len(nums)
 
         def find(x):
-            cx = x
             if x % 2 == 0:
                 return -1
             i = 0
             while x:
-                if x & 1 == 0:
+                if x & (1 << i) == 0:
                     break
                 i += 1
-                x >>= 1
-            cx = cx & ~(1 << (i - 1))
-            return cx
+            x = x & ~(1 << (i - 1))
+            return x
 
         for i in range(n):
             ans.append(find(nums[i]))
