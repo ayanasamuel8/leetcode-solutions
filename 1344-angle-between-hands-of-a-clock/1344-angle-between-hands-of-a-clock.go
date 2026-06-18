3dccc726f653ba46cc34aebfb0ae7e2e52accda1
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2026-06-18 22:11:40 +0300

    Time: 0 ms (100%), Space: 4 MB (0%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.go
@@ -0,0 +1,10 @@
+func angleClock(hour int, minutes int) float64 {
+
+	minuteAngle := float64(minutes) * 6.0
+
+	hourAngle := (float64(hour) * 30.0) + (float64(minutes) * 0.5)
+
+	diff := math.Abs(hourAngle - minuteAngle)
+
+	return math.Min(diff, 360.0-diff)
+}
