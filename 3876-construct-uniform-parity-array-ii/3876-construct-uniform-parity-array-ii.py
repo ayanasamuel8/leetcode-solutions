8d889e086d549a321ea7135ae93bef76ce8f9779
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2026-09-03 21:15:27 +0300

    Time: 23 ms (76.22%), Space: 35.3 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -2,14 +2,10 @@ class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         odds, evens = [], []
         for num in nums1:
-            if num % 2 == 0:
-                evens.append(num)
-            else:
-                odds.append(num)
-        odds.sort()
-        evens.sort()
-        if odds and evens:
-            if evens[0] > odds[0]:
-                return True
+            if num % 2 != 0:
+                break
+        else:
+            return True
+        if min(nums1) % 2 == 0:
             return False
         return True
